c3164ffaf0386bd28e7f20d04e64805ba1537e68
Bekithemba Mdluli <putthetrust.gamedev@gmail.com>
2023-07-21 19:59:04 +0200

    add: post functionality for expense

--- src/pages/expenses.tsx
+++ src/pages/expenses.tsx
@@ -1,11 +1,35 @@
 import { useEffect, useState } from "react";
 import moment from "moment";
 
-import Button from "../components/button";
+// import Button from "../components/button";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 import Card from "../components/card";
 import MobileNav from "../components/mobile-nav";
 import Sidebar from "../components/sidebar";
 import axios from "axios";
+import {
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "../components/ui/dialog";
+import { useForm } from "react-hook-form";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../components/ui/form";
+
+import { Input } from "../components/ui/input";
+import { Button } from "../components/ui/button";
 
 interface ExpenseProps {
   _id: string;
@@ -16,9 +40,50 @@ interface ExpenseProps {
   description: string;
 }
 
+const formSchema = z.object({
+  name: z.string().min(3).max(50),
+  price: z.string(),
+  category: z.string().min(3).max(50),
+  description: z.string().min(3).max(50),
+});
+
 const Expenses = () => {
   const [expenses, setExpenses] = useState<any>();
   const [isLoading, setIsLoading] = useState(false);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "fdfddsdsdsdsddsd",
+      price: "500",
+      category: "dfdfdsdsdsdddsdsf",
+      description: "dfddsdsdsdsddsf",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    if (values) {
+      await axios.post("http://localhost:3000/api/v1/expenses", {
+        data: {
+          name: values.name,
+          price: values.price,
+          category: values.category,
+          description: values.description,
+          userId: "64b8c14935ef2e83200681bb",
+        },
+      });
+    }
+
+    //   {
+    //     "name": "Burger King",
+    //     "price": 49,
+    //     "category": "Entertainment",
+    //     "description": "Watch anime movies, series online",
+    //     "userId": "64b8c14935ef2e83200681bb"
+    // }
+  };
 
   useEffect(() => {
     setIsLoading(true);
@@ -48,7 +113,83 @@ const Expenses = () => {
       <div className="md:ml-[306px] mx-4">
         <div className="flex items-center justify-between mt-4">
           <h1 className="font-bold text-xl md:text-4xl">EXPENSES</h1>
-          <Button text="Add Expense" onclick={() => {}} />
+          {/* <Button text="Add Expense" onclick={() => {}} /> */}
+          <Dialog>
+            <DialogTrigger className="bg-custom-green rounded-full h-[44px] px-[5%] text-white">
+              Open
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Add Expense</DialogTitle>
+                <Form {...form}>
+                  <form
+                    onSubmit={form.handleSubmit(onSubmit)}
+                    className="space-y-8"
+                  >
+                    <FormField
+                      control={form.control}
+                      name="name"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Name</FormLabel>
+                          <FormControl>
+                            <Input placeholder="Crunchyroll" {...field} />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+
+                    <FormField
+                      control={form.control}
+                      name="price"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Price</FormLabel>
+                          <FormControl>
+                            <Input placeholder="49" {...field} />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+
+                    <FormField
+                      control={form.control}
+                      name="category"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Category</FormLabel>
+                          <FormControl>
+                            <Input placeholder="Entertainment" {...field} />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+
+                    <FormField
+                      control={form.control}
+                      name="description"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Description</FormLabel>
+                          <FormControl>
+                            <Input
+                              placeholder="Watch anime online"
+                              {...field}
+                            />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <Button type="submit">Submit</Button>
+                  </form>
+                </Form>
+              </DialogHeader>
+            </DialogContent>
+          </Dialog>
         </div>
         <div className="grid items-center md:grid-cols-2 xl:grid-cols-3 gap-4">
           {expenses?.map((expense: ExpenseProps) => (
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,12 +1,15 @@
 import MobileNav from "../components/mobile-nav";
 import Sidebar from "../components/sidebar";
+import { Button } from "../components/ui/button";
 
 const Home = () => {
   return (
     <div className="relative">
       <Sidebar />
       <MobileNav />
-      <div className="md:ml-[246px]">HOME</div>
+      <div className="md:ml-[246px]">
+        <Button> Click me</Button>
+      </div>
     </div>
   );
 };

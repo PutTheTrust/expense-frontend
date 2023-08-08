c57c26cf7fe1c95888d87a44ff2175396c4fa32d
Bekithemba <putthetrust.gamedev@gmail.com>
2023-08-09 00:02:44 +0200

    add: logout button to mobile nav

--- src/components/mobile-nav.tsx
+++ src/components/mobile-nav.tsx
@@ -1,7 +1,12 @@
 import { NavLink } from "react-router-dom";
 import { routes } from "../constants";
+import { LogOut } from "lucide-react";
 
 const MobileNav = () => {
+  const logout = () => {
+    localStorage.removeItem("token");
+    window.location.reload();
+  };
   return (
     <div className="bg-dark rounded-full fixed bottom-8 w-[90%] left-[5%] right-[5%] md:hidden">
       <ul className="flex justify-between items-center h-[56px] mx-8">
@@ -17,6 +22,9 @@ const MobileNav = () => {
             </NavLink>
           </li>
         ))}
+        <li onClick={logout} className=" text-white cursor-pointer">
+          <LogOut />
+        </li>
       </ul>
     </div>
   );
